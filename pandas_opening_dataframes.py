fdcb9f5f71ec1f76cbd6ff210b6688703145235a
Dani <54872701+danielleappel@users.noreply.github.com>
2021-05-11 14:15:59 -0400

    Cleaned confirmed and death dfs

--- pandas_opening_dataframes.py
+++ pandas_opening_dataframes.py
@@ -39,7 +39,7 @@ except:
 #######################
 
 # DSHS Hospital Capacity
-dshs_data_over_time_url = "http://dshs.texas.gov/coronavirus/CombinedHospitalDataoverTimebyTSA.xlsx"
+dshs_data_over_time_url = "https://dshs.texas.gov/coronavirus/CombinedHospitalDataoverTimebyTSA.xlsx"
 dshs_hospital_capacity_df = pd.read_excel(dshs_data_over_time_url, header=2, sheet_name="GA-32 COVID % Capacity")
 
 dshs_hospital_capacity_df = dshs_hospital_capacity_df.iloc[22:23, 2:]  # Eliminate unnecessary rows and columns (that do not contain data)
@@ -97,22 +97,27 @@ for row in cur.execute("SELECT * FROM business_apps"):
     pass
     #print(row)
 
-
 #######################
 ##### COUNTY LEVEL#####
 #######################
 
 # Confirmed Cases - Github Data
-confirmed_url = "http://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+confirmed_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
 confirmed_df = pd.read_csv(confirmed_url)
+
+confirmed_df = confirmed_df[confirmed_df.iso2 == "US"] # Drop rows of data outside of the US
+confirmed_df = confirmed_df.drop(columns=["UID", "iso2", "iso3", "FIPS", "code3", "Country_Region", "Lat", "Long_", "Combined_Key"]) # Drop columns that are unnecessary
+
 print(confirmed_df)
-#print(confirmed_df["Admin2"])
-    # Admin2 = county
 
 # Death Cases - Github Data
 death_cases_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
 death_cases_df = pd.read_csv(death_cases_url)
-#print(death_cases_df)
+
+death_cases_df = death_cases_df[death_cases_df.iso2 == "US"] # Drop rows of data outside of the US
+death_cases_df = death_cases_df.drop(columns=["UID", "iso3", "FIPS", "code3", "Country_Region", "Lat", "Long_", "Combined_Key"]) # Drop columns that are unnecessary
+
+print(death_cases_df)
 
 # Unemployment Claims
 unemployment_url = "https://www.twc.texas.gov/files/agency/weekly-claims-by-county-twc.xlsx"
@@ -121,6 +126,8 @@ unemployment_df = pd.read_excel(unemployment_url, header=2)
 unemployment_df = unemployment_df.iloc[:254, :]             # Eliminate unnecessary rows (that do not contain data)
 
 unemployment_df = unemployment_df.dropna(axis=1, how="all") # Drop any columns that are all NaN
+
+print(unemployment_df)
 
 column_names_dshs = ["["+ str(col) + "]" for col in unemployment_df.columns] # Generate a the column declaration for the table
 column_declaration = "County text, " + " int, ".join(column_names_dshs[1:]) + " int"
