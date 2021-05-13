f75d0ec71397bb0d4286abbc8b760ee79a51c1ea
Dani <54872701+danielleappel@users.noreply.github.com>
2021-05-12 20:34:18 -0400

    Created the county unemployment table

--- pandas_opening_dataframes.py
+++ pandas_opening_dataframes.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sqlite3
 import re
-from datetime import datetime, timedelta, date
+import datetime
 import math
 
 con = sqlite3.connect('example.db')
@@ -52,7 +52,7 @@ dshs_hospital_capacity_df = dshs_hospital_capacity_df.iloc[22:23, 2:]  # Elimina
 
 cur.execute("CREATE TABLE texas_capacity (Date TEXT, CovidHospOutOfCapacity FLOAT)") # Create table
 
-clean_date = date(2020, 4, 11) # Data begins on April 11, 2020. 
+clean_date = datetime.date(2020, 4, 11) # Data begins on April 11, 2020. 
                                # Use this date to generate clean date entries since several have errors
 
 for i in dshs_hospital_capacity_df:
@@ -61,7 +61,7 @@ for i in dshs_hospital_capacity_df:
     cur.execute("INSERT INTO texas_capacity (Date, CovidHospOutOfCapacity) VALUES('{}', {})".format(clean_date, capac))
                                                  # Add the entry to the table
 
-    clean_date = clean_date + timedelta(days=1)  # Icrement the date
+    clean_date = clean_date + datetime.timedelta(days=1)  # Icrement the date
 
 #for row in cur.execute("SELECT Date, CovidHospOutOfCapacity FROM texas_capacity"):
     #pass
@@ -84,7 +84,7 @@ dshs_icu_bed_utilization_df = dshs_covid_icu_beds_df/(dshs_covid_icu_beds_df + d
 
 cur.execute("CREATE TABLE texas_icu_utilization (Date TEXT, ICU_Utilization INTEGER)") # Create table
 
-clean_date = date(2020, 4, 11) # Data begins on April 11, 2020. 
+clean_date = datetime.date(2020, 4, 11) # Data begins on April 11, 2020. 
                                # Use this date to generate clean date entries since several have errors
 
 for i in dshs_icu_bed_utilization_df:
@@ -93,7 +93,7 @@ for i in dshs_icu_bed_utilization_df:
     cur.execute("INSERT INTO texas_icu_utilization (Date, ICU_Utilization) VALUES('{}', {})".format(clean_date, utilization))
                                                   # Add the entry to the table
 
-    clean_date = clean_date + timedelta(days=1)   # Icrement the date
+    clean_date = clean_date + datetime.timedelta(days=1)   # Icrement the date
 
 #for row in cur.execute("SELECT * FROM texas_icu_utilization"):
     #pass
@@ -122,7 +122,7 @@ day = 1 # Enter all monthly data on the first of the corresponding month
 for i, row in business_app_df.iterrows():
     year = row[0]
     for month in range(1,12):
-        d = date(year, month, day)
+        d = datetime.date(year, month, day)
         apps = row[month]
 
         # Only add entries that are not null
@@ -150,7 +150,7 @@ cur.execute("CREATE TABLE confirmed_by_county (County TEXT, Date TEXT, Confirmed
 
 # Add entries to the table
 for index, row in confirmed_df.iterrows():
-    clean_date = date(2020, 1, 22) # Data begins on January 22, 2020
+    clean_date = datetime.date(2020, 1, 22) # Data begins on January 22, 2020
     county = row[0]
 
     for col in range(1,len(row)):
@@ -170,13 +170,11 @@ death_cases_df = pd.read_csv(death_cases_url)
 death_cases_df = death_cases_df[death_cases_df.Province_State == "Texas"] # Drop rows of data outside of the US
 death_cases_df = death_cases_df.drop(columns=["UID", "Province_State", "iso2", "iso3", "FIPS", "code3", "Country_Region", "Lat", "Long_", "Population", "Combined_Key"]) # Drop columns that are unnecessary
 
-print(death_cases_df)
-
 cur.execute("CREATE TABLE deaths_by_county (County TEXT, Date TEXT, Deaths INTEGER)")  # Create table
 
 # Add entries to the table
 for index, row in death_cases_df.iterrows():
-    clean_date = date(2020, 1, 22) # Data begins on January 22, 2020
+    clean_date = datetime.date(2020, 1, 22) # Data begins on January 22, 2020
     county = row[0]
 
     for col in range(1,len(row)):
@@ -195,11 +193,39 @@ unemployment_df = pd.read_excel(unemployment_url, header=2)
 
 unemployment_df = unemployment_df.iloc[:254, :]             # Eliminate unnecessary rows (that do not contain data)
 unemployment_df = unemployment_df.dropna(axis=1, how="all") # Drop any columns that are all NaN
+unemployment_df = unemployment_df.fillna("NULL")            # And replace all NaN with Null
 
-column_names_dshs = ["["+ str(col) + "]" for col in unemployment_df.columns] # Generate a the column declaration for the table
-column_declaration = "County text, " + " int, ".join(column_names_dshs[1:]) + " int"
+cur.execute("CREATE TABLE unemployment_by_county (County TEXT, Date TEXT, UnemploymentClaims INTEGER)") # Create table
 
-cur.execute("CREATE TABLE county_unemployment (" + column_declaration + ")") # Create table
+# Add entries to the table
+for index, row in unemployment_df.iterrows():
+    county = row[0]
+
+    for col in range(1,len(row)):
+        d = unemployment_df.columns[col]
+
+        # If the date is not a datetime object, make it one
+        if not isinstance(d, datetime.datetime):
+            d = datetime.datetime.strptime(d, r"%m/%d/%Y")
+        d = d.date()
+
+        unemp_claims = row[col]
+
+        # Only add entries that are not null
+        if unemp_claims != "NULL":
+            cur.execute("INSERT INTO unemployment_by_county (County, Date, UnemploymentClaims) VALUES('{}', '{}', {})".format(county, d, unemp_claims))
+
+
+#for row in cur.execute("SELECT * FROM unemployment_by_county"):
+    #pass
+    #print(row)
+
+
+
+
+
+
+
 
 #for row in cur.execute("SELECT * FROM county_unemployment"):
     #pass
